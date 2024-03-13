ad421a08a0c2313d474c712e4956b49dd49b6290
Jesus <jesus.rangel@cceo.com.mx>
2024-03-13 14:06:00 -0600

    feat: :sparkles: Created lista_noticias.dart widget to display a list of mexican news, modified tab1_page to send the necessary params for the widget.

--- lib/src/pages/tab1_page.dart
+++ lib/src/pages/tab1_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:newsapp/src/services/news_service.dart';
+import 'package:newsapp/src/widgets/lista_noticias.dart';
 import 'package:provider/provider.dart';
 
 class Tab1Page extends StatelessWidget {
@@ -9,6 +10,8 @@ class Tab1Page extends StatelessWidget {
   Widget build(BuildContext context) {
     
     final newsService = Provider.of<NewsService>(context);
-    return  const Center(child: Text('hola xd'));
+    return Scaffold(
+      body: ListaNoticias(newsService.headlines),
+    );
   }
 }
---
+++ lib/src/widgets/lista_noticias.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/src/models/news_models.dart';
+import 'package:newsapp/src/theme/darktheme.dart';
+
+class ListaNoticias extends StatelessWidget {
+  
+  final List<Article> noticias;
+
+  const ListaNoticias(this.noticias);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: noticias.length,
+      itemBuilder: (BuildContext context, int index){
+        return _Noticia(noticia: noticias[index], index: index);
+      }
+    );
+  }
+}
+
+class _Noticia extends StatelessWidget {
+  final Article noticia;
+  final int index;
+
+  const _Noticia({required this.noticia, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _TarjetaTopBar(noticia: noticia, index: index),
+        _TarjetaTitulo(noticia: noticia),
+        _TarjetaImagen(noticia: noticia),
+      ],
+    );
+  }
+}
+
+class _TarjetaTopBar extends StatelessWidget {
+  final Article noticia;
+  final int index;
+
+  const _TarjetaTopBar({required this.noticia, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      margin: const EdgeInsets.only(bottom: 10),
+      child: Row(
+        children: <Widget>[
+          Text('${index+1}. ', style: TextStyle(color: myTheme.colorScheme.secondary)),
+          Text('${noticia.source.name}. '),
+        ],
+      ),
+    );
+  }
+}
+
+class _TarjetaTitulo extends StatelessWidget {
+  final Article noticia;
+
+  const _TarjetaTitulo({required this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(noticia.title, style: const TextStyle(fontSize: 20,fontWeight: FontWeight.w700),),
+    );
+    }
+}
+
+class _TarjetaImagen extends StatelessWidget {
+  final Article noticia;
+
+  const _TarjetaImagen({required this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text('Hola mundin'),
+    );
+  }
+}
