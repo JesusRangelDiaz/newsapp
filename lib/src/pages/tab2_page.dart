e08fe30ab6a4774ad3699f4f3e1a63c181b50adf
Jesus <jesus.rangel@cceo.com.mx>
2024-03-14 18:17:13 -0600

    feat: :sparkles: The second section of the bottomtabnavigator was designed in tab2_page. Added an API connection to get the news by category in news_service.dart. Added widgets in tab2_page to show a categories menu and its included functions.

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:newsapp/src/models/category_model.dart';
 import 'package:newsapp/src/services/news_service.dart';
+import 'package:newsapp/src/theme/darktheme.dart';
+import 'package:newsapp/src/widgets/lista_noticias.dart';
 import 'package:provider/provider.dart';
 
 class Tab2Page extends StatelessWidget {
@@ -9,11 +11,15 @@ class Tab2Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final newsService =Provider.of<NewsService>(context);
+
     return SafeArea(
       child: Scaffold(
         body: Column(
           children: <Widget>[
-            Expanded(child: _CategoryList())
+            _CategoryList(),
+            Expanded(child: ListaNoticias(newsService.getArticulosCategoriaSeleccionada))
           ],
         ),
       ),
@@ -27,24 +33,28 @@ class _CategoryList extends StatelessWidget {
   Widget build(BuildContext context) {
     final categories = Provider.of<NewsService>(context).categories;
 
-    return ListView.builder(
-      physics: const BouncingScrollPhysics(),
-      scrollDirection: Axis.horizontal,
-      itemCount: categories.length,
-      itemBuilder: (BuildContext context, int index){
-        final cName = categories[index].name;
-        return Padding(
-          padding: const EdgeInsets.all(8),
-          child: Column(
-            children: <Widget>[
-              _CategoryButton(categoria: categories[index]),
-              const SizedBox(height: 5),
-              Text('${cName[0].toUpperCase() }${ cName.substring(1)}')
-            ],
-          ),
-        );
-      }
-      );
+    return Container(
+      width: double.infinity,
+      height: 81,
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (BuildContext context, int index){
+          final cName = categories[index].name;
+          return Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              children: <Widget>[
+                _CategoryButton(categoria: categories[index]),
+                const SizedBox(height: 5),
+                Text('${cName[0].toUpperCase() }${ cName.substring(1)}')
+              ],
+            ),
+          );
+        }
+        ),
+    );
   }
 }
 
@@ -56,14 +66,19 @@ class _CategoryButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final newsService = Provider.of<NewsService>(context,listen: false);
     return GestureDetector(
-      onTap: (){print(categoria.name);},
+      onTap: (){
+        final newsService = Provider.of<NewsService>(context,listen: false);
+        newsService.selectedCategory=categoria.name;
+
+      },
       child: Container(
         width: 40,
         height: 40,
         margin: const EdgeInsets.symmetric(horizontal: 10),
         decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.white),
-        child: Icon(categoria.icon, color: Colors.black54),
+        child: Icon(categoria.icon, color: (newsService.selectedCategory == categoria.name)?myTheme.colorScheme.secondary : Colors.black54),
       ),
     );
   }
